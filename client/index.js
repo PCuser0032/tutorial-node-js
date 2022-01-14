4905f5d08367b914ab21db7470dfed76e1f2f3dd
user0032 <70772813+PCuser0032@users.noreply.github.com>
2022-01-14 22:25:56 +0300

    edit index.js

--- client/index.js
+++ client/index.js
@@ -1,5 +1,23 @@
-const requestURL = 'https://localhost:7000/about';
-const xhr = new XMLHttpRequest();
-xhr.open('GET', requestURL);
-xhr.send();
-console.log("client");
+const requestURL = "https://jsonplaceholder.typicode.com/users";
+
+function sendRequest(method, url) {
+  return new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+
+    xhr.onload = function () {
+      console.log(JSON.parse(xhr.response));
+    };
+
+    xhr.send();
+  });
+}
+
+sendRequest('GET', requestURL)
+.then(function(data) {
+  return console.log(data);
+})
+.catch(function(err) {
+  return console.log(err);
+})
